6a488877b5124995e1659007ad43f9133c0ac8e4
Az21 <39803478+Az-21@users.noreply.github.com>
2021-06-14 13:56:26 +0530

    🚀 sveltekit + tailwind + vercel stack init

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<p class="pt-10 flex text-3xl justify-center font-mono text-green-600">Az-21/digital-payment-awareness</p>
+
+<style>
+	:global(body) {
+		background-color: #1a1a1a;
+		color: #fff;
+	}
+</style>
