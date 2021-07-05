53a549dcb8df7e8f983e0a5cfa38e906156d083e
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-06 01:26:17 +0530

    🌟 added section header component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,12 +3,16 @@
 	import Navbar from '../components/Navbar.svelte';
 	import Hero from '../components/Hero.svelte';
 	import CTA from '../components/CTA.svelte';
+	import SectionHeader from '../components/SectionHeader.svelte';
 </script>
 
 <!-- HTML Components -->
 <Navbar />
 <Hero />
 <CTA />
+
+<!-- Blog section -->
+<SectionHeader text="Blog" textColor="text-blue-500" divColor="divide-blue-500" />
 
 <!-- CSS -->
 <style>
