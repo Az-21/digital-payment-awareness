4cb8f8375fc04c5d31c009dc29ae657a068b20fa
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-06 02:51:07 +0530

    📃 added blog card

---
+++ src/components/Blog.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let title: string = 'err: title prop not passed';
+	export let subtitle: string = 'err: subtitle prop not passed';
+	export let img: string = 'err: image prop not passed';
+	export let time: string = 'err: time to read prop not passed';
+</script>
+
+<div class="mx-auto">
+	<div class="bg-gray-900 shadow-md rounded-lg mb-6 tracking-wide">
+		<div class="md:flex-shrink-0">
+			<img src={img} alt="cover" class="w-full h-64" />
+		</div>
+		<div class="px-4 py-2 mt-2">
+			<h2 class="font-bold text-2xl tracking-normal">{title}</h2>
+			<p class="text-sm text-gray-300 h-32 overflow-hidden">{subtitle}</p>
+			<p class="text-gray-500 text-xs pt-2">{time}</p>
+			<div class="mt-4 flex items-center">
+				<i class="fas fa-angle-down text-blue-500 mr-2 transform scale-150" />
+				<a href="#todo:route" class="text-blue-500">Read More</a>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,9 @@
 	import Hero from '../components/Hero.svelte';
 	import CTA from '../components/CTA.svelte';
 	import SectionHeader from '../components/SectionHeader.svelte';
+	import Blog from '../components/Blog.svelte';
+	import { blog } from '../content/BlogJSON.svelte';
+	import { estimateTimeToRead, truncateText } from '../components/Functions.svelte';
 </script>
 
 <!-- HTML Components -->
@@ -13,6 +16,16 @@
 
 <!-- Blog section -->
 <SectionHeader text="Blog" textColor="text-blue-500" divColor="divide-blue-500" />
+<div class="w-5/6 mx-auto grid md:grid-cols-3 gap-4">
+	{#each blog as blog}
+		<Blog
+			title={blog.title}
+			subtitle={truncateText(blog.content)}
+			img={blog.img}
+			time={estimateTimeToRead(blog.content)}
+		/>
+	{/each}
+</div>
 
 <!-- CSS -->
 <style>
