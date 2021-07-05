9b077bfa20eac251ca18b5dd8359fec7f1deede0
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-06 00:12:00 +0530

    ㊙ section comments

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,12 @@
+<!-- Typescript -->
 <script lang="ts">
 	import Navbar from '../components/Navbar.svelte';
 </script>
 
+<!-- HTML Components -->
 <Navbar />
 
+<!-- CSS -->
 <style>
 	:global(body) {
 		background-color: #1a1a1a;
