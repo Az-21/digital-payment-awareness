3e2b2f3f9db9449fa8db872b82b391239c54b995
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-06 01:03:22 +0530

    💎 updated to dark background and reduced elevation

--- src/components/CTA.svelte
+++ src/components/CTA.svelte
@@ -2,11 +2,11 @@
 	<div class="flex flex-wrap">
 		<div class="w-full md:w-4/12 px-4 text-center">
 			<div
-				class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+				class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-md rounded-lg"
 			>
-				<a href="#todo:jump" class="px-4 py-5 flex-auto">
+				<a href="#todo:jumpToSection" class="px-4 py-5 flex-auto bg-gray-900">
 					<div
-						class="p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-lg bg-blue-500"
+						class="p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-md rounded-lg bg-blue-500"
 					>
 						<i class="fas fa-file-alt" />
 					</div>
@@ -19,11 +19,11 @@
 		</div>
 		<div class="w-full md:w-4/12 px-4 text-center">
 			<div
-				class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+				class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-md rounded-lg"
 			>
-				<a href="#todo:jump" class="px-4 py-5 flex-auto">
+				<a href="#todo:jumpToSection" class="px-4 py-5 flex-auto bg-gray-900">
 					<div
-						class="p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-lg bg-yellow-500"
+						class="p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-md rounded-lg bg-yellow-500"
 					>
 						<i class="fas fa-newspaper" />
 					</div>
@@ -36,11 +36,11 @@
 		</div>
 		<div class="w-full md:w-4/12 px-4 text-center">
 			<div
-				class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+				class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-md rounded-lg"
 			>
-				<a href="#todo:jump" class="px-4 py-5 flex-auto">
+				<a href="#todo:jumpToSection" class="px-4 py-5 flex-auto bg-gray-900">
 					<div
-						class="p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-lg bg-red-500"
+						class="p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-md rounded-lg bg-red-500"
 					>
 						<i class="fab fa-youtube" />
 					</div>
