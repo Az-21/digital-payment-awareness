1bb7efc5e7eccaec0a72933f6fae40e390a7b86d
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-06 02:50:11 +0530

    🎉 added functions to truncate text and estimate time to read

---
+++ src/components/Functions.svelte
@@ -0,0 +1,12 @@
+<script lang="ts" context="module">
+	/// Get the 'n minutes read' estimate
+	export function estimateTimeToRead(text: string): string {
+		let time = Math.round(text.match(/\s/g).length / 40);
+		return `${time} Minute Read`;
+	}
+
+	/// Get the first `n` words from a string and append with `...`
+	export function truncateText(text: string): string {
+		return `${text.substr(0, 300)} ...`;
+	}
+</script>
