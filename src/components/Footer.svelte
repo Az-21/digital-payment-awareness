a6c72b9e9e9f3bf0a70ce6d3cc7a0d415ad89f78
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-06 12:26:20 +0530

    📱 optimized footer for phone

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,16 +1,12 @@
 <footer class="relative bg-gray-800 pt-8 pb-6">
-	<div
-		class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
-		style="height: 80px;"
-	/>
-	<div class="container mx-auto px-4">
+	<div class="container mx-auto w-5/6">
 		<div class="flex flex-wrap">
-			<div class="w-full lg:w-6/12 px-4">
+			<div class="w-full lg:w-6/12">
 				<h4 class="text-3xl font-semibold">Lorem ipsum</h4>
 				<h5 class="text-lg mt-0 mb-2 text-gray-400">
 					// todo: add some closing remark
 				</h5>
-				<div class="mt-6">
+				<div class="mt-6 pt-4 lg:pt-0">
 					<button
 						class="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
 						type="button"
@@ -34,9 +30,9 @@
 					</button>
 				</div>
 			</div>
-			<div class="w-full lg:w-6/12 px-4">
+			<div class="w-full lg:w-6/12">
 				<div class="flex flex-wrap items-top mb-6">
-					<div class="w-full lg:w-4/12 px-4 ml-auto">
+					<div class="w-1/2 lg:w-2/5 ml-auto pt-4 lg:pt-0">
 						<span class="block uppercase text-gray-100 text-sm font-semibold mb-2"
 							>Useful Links</span
 						>
@@ -67,8 +63,8 @@
 							</li>
 						</ul>
 					</div>
-					<div class="w-full lg:w-4/12 px-4">
-						<span class="block uppercase text-gray-100 text-sm font-semibold mb-2"
+					<div class="w-1/2 lg:w-1/5">
+						<span class="block uppercase text-gray-100 text-sm font-semibold mb-2 pt-4 lg:pt-0"
 							>Useful Services</span
 						>
 						<ul class="list-unstyled">
