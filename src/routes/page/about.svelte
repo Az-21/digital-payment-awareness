bc2bd5f575cb51a1ebb6a904b5f0319034bce110
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-08 00:13:54 +0530

    ℹ added about us section

---
+++ src/routes/page/about.svelte
@@ -0,0 +1,301 @@
+<script lang="ts">
+    // Svelte component
+    import Footer from '../../components/Footer.svelte';
+</script>
+
+<div class="pt-12 container mx-auto px-4">
+	<div class="flex flex-wrap justify-center text-center mb-12">
+		<div class="w-full lg:w-6/12 px-4">
+			<h2 class="text-4xl font-semibold">Meet the Team</h2>
+			<p class="text-lg leading-relaxed text-gray-600">
+				//todo: something about us as a collective
+			</p>
+		</div>
+	</div>
+	<div class="flex flex-wrap">
+		<div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+			<div class="px-6">
+				<img
+					alt="memberPhoto"
+					src="https://via.placeholder.com/150"
+					class="shadow-lg rounded-full max-w-full mx-auto"
+					style="max-width: 120px;"
+				/>
+				<div class="pt-6 text-center">
+					<p class="text-xl font-bold">ABC XYZ</p>
+					<p class="mt-1 text-sm text-gray-500 uppercase font-semibold">Contribution</p>
+					<div class="mt-2">
+						<button
+							class="bg-blue-400 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-twitter" />
+						</button>
+						<button
+							class="bg-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-github text-gray-900 transform scale-125" />
+						</button>
+						<button
+							class="bg-red-500 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fas fa-envelope" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+			<div class="px-6">
+				<img
+					alt="memberPhoto"
+					src="https://via.placeholder.com/150"
+					class="shadow-lg rounded-full max-w-full mx-auto"
+					style="max-width: 120px;"
+				/>
+				<div class="pt-6 text-center">
+					<p class="text-xl font-bold">Abdul Baquar</p>
+					<p class="mt-1 text-sm text-gray-500 uppercase font-semibold">Editor and Writer</p>
+					<div class="mt-2">
+						<button
+							class="bg-blue-400 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-twitter" />
+						</button>
+						<button
+							class="bg-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-github text-gray-900 transform scale-125" />
+						</button>
+						<button
+							class="bg-red-500 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fas fa-envelope" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+			<div class="px-6">
+				<img
+					alt="memberPhoto"
+					src="https://via.placeholder.com/150"
+					class="shadow-lg rounded-full max-w-full mx-auto"
+					style="max-width: 120px;"
+				/>
+				<div class="pt-6 text-center">
+					<p class="text-xl font-bold">Abhishek Choudhary</p>
+					<p class="mt-1 text-sm text-gray-500 uppercase font-semibold">Fullstack Developer</p>
+					<div class="mt-2">
+						<button
+							class="bg-blue-400 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-twitter" />
+						</button>
+						<button
+							class="bg-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-github text-gray-900 transform scale-125" />
+						</button>
+						<button
+							class="bg-red-500 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fas fa-envelope" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+			<div class="px-6">
+				<img
+					alt="memberPhoto"
+					src="https://via.placeholder.com/150"
+					class="shadow-lg rounded-full max-w-full mx-auto"
+					style="max-width: 120px;"
+				/>
+				<div class="pt-6 text-center">
+					<p class="text-xl font-bold">ABC XYZ</p>
+					<p class="mt-1 text-sm text-gray-500 uppercase font-semibold">Contribution</p>
+					<div class="mt-2">
+						<button
+							class="bg-blue-400 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-twitter" />
+						</button>
+						<button
+							class="bg-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-github text-gray-900 transform scale-125" />
+						</button>
+						<button
+							class="bg-red-500 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fas fa-envelope" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="flex flex-wrap pt-4">
+		<div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+			<div class="px-6">
+				<img
+					alt="memberPhoto"
+					src="https://via.placeholder.com/150"
+					class="shadow-lg rounded-full max-w-full mx-auto"
+					style="max-width: 120px;"
+				/>
+				<div class="pt-6 text-center">
+					<p class="text-xl font-bold">ABC XYZ</p>
+					<p class="mt-1 text-sm text-gray-500 uppercase font-semibold">Contribution</p>
+					<div class="mt-2">
+						<button
+							class="bg-blue-400 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-twitter" />
+						</button>
+						<button
+							class="bg-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-github text-gray-900 transform scale-125" />
+						</button>
+						<button
+							class="bg-red-500 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fas fa-envelope" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+			<div class="px-6">
+				<img
+					alt="memberPhoto"
+					src="https://via.placeholder.com/150"
+					class="shadow-lg rounded-full max-w-full mx-auto"
+					style="max-width: 120px;"
+				/>
+				<div class="pt-6 text-center">
+					<p class="text-xl font-bold">ABC XYZ</p>
+					<p class="mt-1 text-sm text-gray-500 uppercase font-semibold">Contribution</p>
+					<div class="mt-2">
+						<button
+							class="bg-blue-400 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-twitter" />
+						</button>
+						<button
+							class="bg-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-github text-gray-900 transform scale-125" />
+						</button>
+						<button
+							class="bg-red-500 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fas fa-envelope" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+			<div class="px-6">
+				<img
+					alt="memberPhoto"
+					src="https://via.placeholder.com/150"
+					class="shadow-lg rounded-full max-w-full mx-auto"
+					style="max-width: 120px;"
+				/>
+				<div class="pt-6 text-center">
+					<p class="text-xl font-bold">ABC XYZ</p>
+					<p class="mt-1 text-sm text-gray-500 uppercase font-semibold">Contribution</p>
+					<div class="mt-2">
+						<button
+							class="bg-blue-400 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-twitter" />
+						</button>
+						<button
+							class="bg-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-github text-gray-900 transform scale-125" />
+						</button>
+						<button
+							class="bg-red-500 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fas fa-envelope" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+			<div class="px-6">
+				<img
+					alt="memberPhoto"
+					src="https://via.placeholder.com/150"
+					class="shadow-lg rounded-full max-w-full mx-auto"
+					style="max-width: 120px;"
+				/>
+				<div class="pt-6 text-center">
+					<p class="text-xl font-bold">ABC XYZ</p>
+					<p class="mt-1 text-sm text-gray-500 uppercase font-semibold">Contribution</p>
+					<div class="mt-2">
+						<button
+							class="bg-blue-400 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-twitter" />
+						</button>
+						<button
+							class="bg-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+							type="button"
+						>
+							<i class="fab fa-github text-gray-900 transform scale-125" />
+						</button>
+						<button
+							class="bg-red-500 w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-24"
+							type="button"
+						>
+							<i class="fas fa-envelope" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<Footer />
+
+<!-- CSS -->
+<style>
+	:global(body) {
+		background-color: #1a1a1a;
+		color: #fff;
+	}
+</style>
