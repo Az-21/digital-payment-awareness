660e833b8aa99e9a92ff3f55e746179a7f075538
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-07 15:43:13 +0530

    🔁 added previous | home | next links

--- src/routes/article/[slug].svelte
+++ src/routes/article/[slug].svelte
@@ -6,11 +6,15 @@
 	export async function load(context) {
 		let slug = context.page.params.slug;
 		let post = article[parseInt(slug)];
-		return { props: { slug, post } };
+		let pageID: number = parseInt(slug);
+		let numberOfArticles: number = article.length;
+		return { props: { slug, post, pageID, numberOfArticles } };
 	}
 </script>
 
 <script lang="ts">
+	export let pageID: number;
+	export let numberOfArticles: number;
 	export let post: { content: string; title: any; date: any; img: string };
 
 	// Components
@@ -34,6 +38,34 @@
 	{#each listContent as paragraph}
 		<p class="pt-8 font-serif text-xl tracking-wide">{paragraph}</p>
 	{/each}
+</div>
+
+<!-- Next/Previous Section -->
+<div class="w-5/6 lg:w-3/5 mx-auto pb-24 flex justify-between">
+	<div>
+		{#if pageID != 0}
+			<a href="./{pageID - 1}" class="cursor-pointer pl-2 mt-4 flex items-center">
+				<i class="fas fa-angle-left text-yellow-500 mr-2 transform scale-150" />
+				<p class="text-yellow-500">Previous Article</p>
+			</a>
+		{/if}
+	</div>
+
+	<div>
+		<a href="../" class="cursor-pointer pl-2 mt-4 flex items-center">
+			<i class="fas fa-home text-yellow-500 mr-2 transform scale-150" />
+			<p class="text-yellow-500">Home</p>
+		</a>
+	</div>
+
+	<div>
+		{#if pageID + 1 != numberOfArticles}
+			<a href="./{pageID + 1}" class="cursor-pointer pl-2 mt-4 flex items-center">
+				<i class="fas fa-angle-right text-yellow-500 mr-2 transform scale-150" />
+				<p class="text-yellow-500">Next Article</p>
+			</a>
+		{/if}
+	</div>
 </div>
 
 <Footer />
--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -6,11 +6,15 @@
 	export async function load(context) {
 		let slug = context.page.params.slug;
 		let post = blog[parseInt(slug)];
-		return { props: { slug, post } };
+		let pageID: number = parseInt(slug);
+		let numberOfBlogs: number = blog.length;
+		return { props: { slug, post, pageID, numberOfBlogs } };
 	}
 </script>
 
 <script lang="ts">
+	export let pageID: number;
+	export let numberOfBlogs: number;
 	export let post: { content: string; title: any; date: any; img: string };
 
 	// Components
@@ -27,13 +31,41 @@
 <!-- HTML -->
 <div class="w-5/6 lg:w-3/5 mx-auto pb-24">
 	<p class="text-5xl pt-12 lg:pt-24 font-bold">{post.title}</p>
-	<p class="pt-4 text-sm font-medium text-yellow-500">
+	<p class="pt-4 text-sm font-medium text-blue-500">
 		{estimateTimeToRead(post.content)} • Published {post.date}
 	</p>
 	<img class="pt-4 h-64 w-full object-cover" src={post.img} alt="" />
 	{#each listContent as paragraph}
 		<p class="pt-8 font-serif text-xl tracking-wide">{paragraph}</p>
 	{/each}
+</div>
+
+<!-- Next/Previous Section -->
+<div class="w-5/6 lg:w-3/5 mx-auto pb-24 flex justify-between">
+	<div>
+		{#if pageID != 0}
+			<a href="./{pageID - 1}" class="cursor-pointer pl-2 mt-4 flex items-center">
+				<i class="fas fa-angle-left text-blue-500 mr-2 transform scale-150" />
+				<p class="text-blue-500">Previous Blog</p>
+			</a>
+		{/if}
+	</div>
+
+	<div>
+		<a href="../" class="cursor-pointer pl-2 mt-4 flex items-center">
+			<i class="fas fa-home text-blue-500 mr-2 transform scale-150" />
+			<p class="text-blue-500">Home</p>
+		</a>
+	</div>
+
+	<div>
+		{#if pageID + 1 != numberOfBlogs}
+			<a href="./{pageID + 1}" class="cursor-pointer pl-2 mt-4 flex items-center">
+				<i class="fas fa-angle-right text-blue-500 mr-2 transform scale-150" />
+				<p class="text-blue-500">Next Blog</p>
+			</a>
+		{/if}
+	</div>
 </div>
 
 <Footer />
