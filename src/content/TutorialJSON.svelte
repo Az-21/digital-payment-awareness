a93b069dcc842714fcdb1bf0dd7a9d5b19321e81
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-19 19:34:28 +0530

    ➖ removed period from titles

--- src/content/TutorialJSON.svelte
+++ src/content/TutorialJSON.svelte
@@ -1,21 +1,21 @@
 <script lang="ts" context="module">
 	export const tutorial = [
 		{
-			title: 'How to create an account on Google Pay and connect it to your bank account.',
+			title: 'How to create an account on Google Pay and connect it to your bank account',
 			preview: '// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
 			content: `<ul><li>Step 1 : Open GPay on your mobile.</li><li>Step 2 : Enter the phone number that is linked to your bank account and click on <code>Next</code> at the bottom of the screen. </li><li>Step 3 : Select the email and the mobile number you wish to link to your bank account. Tap <code>Next</code>.</li><li>Step 4 : You will receive an OTP on your registered mobile number. Enter the OTP and press <code>Next</code>.</li><li>Step 5 : Click on create UPI pin and select <code>Continue</code>. </li><li>Step 6 : Set a strong pin. This pin is required whenever you open the app. Tap on ✅.</li><li>Step 7 : Now, tap on your profile picture at the top-right corner. </li><li>Step 8 : Click on <code>Add bank account</code>.</li><li>Step 9 : You will get a list of banks. Select the bank in which you have your account.</li><li>Step 10 : Once the bank is selected, a pop-up appears that asks to verify your number with the bank. Click on Continue.</li><li>Step 11 : GPay will send a verification code from your phone and it will auto-verify your number with the bank.</li><li>Step 12 : Now, you need to verify your account with the help of an ATM card. Click on the card and then <code>Start</code> at the bottom of the screen.</li><li>Step 13 : Enter the last 6 digits of your debit card, expiry date and click on ➡.</li><li>Step 14 : You will receive an OTP. Enter it and set a strong UPI pin. This will be required whenever you make a transaction.</li><li>Step 15 : Your account is created and linked to GPay.</li></ul>`,
 			img: 'https://images.unsplash.com/photo-1522937335625-b87ea99dc133?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
 			date: 'July 19, 2021'
 		},
 		{
-			title: 'How to share your UPI ID.',
+			title: 'How to share your UPI ID',
 			preview: '// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
 			content: `<ul><li>Step 1 : Click on your profile picture at the top-right of your screen.</li><li>Step 2 : Click on your profile picture again.</li><li>Step 3 : You will see a screen displaying your <code>Spot Code</code>. Spot Code is similar to a QR code, but is unique to Google Pay. <code>Scan my spot code to connect</code> will be written below the visual code.</li><li>Step 4 : Swipe right. </li><li>Step 5 : You will see a screen with your QR code displayed.</li><li>Step 6 : Below the QR code, you have your UPI ID. </li><li>Step 7 : Copy it and share your UPI ID.</li></ul>`,
 			img: 'https://images.unsplash.com/photo-1530989166579-96a5923c8ce5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80',
 			date: 'July 20, 2021'
 		},
 		{
-			title: 'How to change UPI pin.',
+			title: 'How to change UPI pin',
 			preview:
 				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
 			content: `<ul><li>Step 1 : Click on your profile picture.</li><li>Step 2 : Click on <code>Bank Account</code> under the tab <code>Set up Payment Methods</code>. </li><li>Step 3 : Click on your bank account. </li><li>Step 4 : Tap on the three dots at the top-right corner of your screen.</li><li>Step 5 : Click on <code>Change UPI pin</code>.</li><li>Step 6 : Enter your old UPI pin and press ✅.</li><li>Step 7 : Enter the new pin you wish to keep and press ✅.</li><li>Step 8 : Enter the new pin again to confirm. </li><li>Step 9 : Your UPI pin is changed.</li></ul>`,
@@ -23,12 +23,20 @@
 			date: 'July 21, 2021'
 		},
 		{
-			title: 'How to check account balance.',
+			title: 'How to check account balance',
 			preview:
 				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
 			content: `<ul><li>Step 1 : Click on your profile picture at the top-right of your screen.</li><li>Step 2 : Click on <code>Bank Account</code> under the tab <code>Set up payment methods</code>.</li><li>Step 3 : Click on your bank account.</li><li>Step 4 : At the bottom of the options displayed, you will find <code>View Account Balance</code>. Select that.</li><li>Step 5 : GPay will prompt you to enter your 6-digit pin.</li><li>Step 6 : Enter your UPI pin and press ✅. </li><li>Step 7 : Your account and its balance will be displayed.</li></ul>`,
 			img: 'https://images.unsplash.com/photo-1565373679107-344d38dbf734?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
 			date: 'July 22, 2021'
+		},
+		{
+			title: 'How to pay bills',
+			preview:
+				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			content: `<ul><li>Step 1 : Click on <code>New Payments</code> on the home screen of the app.</li><li>Step 2: Now, click on the <code>Bill Payments</code> option.</li><li>Step 3 : Click on <code>Electricity</code>.</li><li>Step 4 : Select your Electricity Supply Corporation. You will see that no account has been linked yet. </li><li>Step 5 : To link your account, Click on <code>Get Started</code>.</li><li>Step 6 : Enter your 10-digit Account ID. You can find this in your electricity bill.</li><li>Step 7 : Enter your <code>Account Name</code>. Note that this is for your own reference. You may, however, skip this field.</li><li>Step 8 : Check whether the information you have entered is correct.</li><li>Step 9 : Now, click on the <code>Link Account</code> option. Once this is done, you will see that your account has been linked and your most recent bill is displayed.</li><li>Step 10 : You can verify this amount with your electricity bill.</li><li>Step 11 : To make the payment, click on <code>Pay Bill</code>.</li><li>Step 12 : Select the bank account for making the payment and click on <code>Proceed to Pay</code>.</li><li>Step 13: Follow the steps of making a payment. This is similar to a normal transaction you would make.</li><li>Step 14: Your electricity bill is paid. Similarly, you can pay bills for other services like Cable, LPG, Broadband and many more. </li></ul>`,
+			img: 'https://images.unsplash.com/photo-1545941962-1b6654eb8072?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1900&q=80',
+			date: 'July 23, 2021'
 		},
 	];
 </script>
