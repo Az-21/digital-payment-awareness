39596d0a4c9e796a6875bf593bc5958d45e887cb
Az21 <39803478+Az-21@users.noreply.github.com>
2021-07-19 19:20:43 +0530

    👩‍🏫 added tutorial/0

--- src/content/TutorialJSON.svelte
+++ src/content/TutorialJSON.svelte
@@ -1,29 +1,24 @@
 <script lang="ts" context="module">
-	// TODO: delete this placeholder when actual content is added
-	const placeholderText: string = `<h1 id="sample-markdown">Sample Markdown</h1><p>Aliquam bibendum ac augue sed consectetur. Nulla nisl ante, iaculis ut velit at, suscipit pellentesque urna. Sed eu risus volutpat, ornare mi eget, scelerisque nisl. Proin mauris sapien, condimentum vitae turpis ac, iaculis porttitor lectus. Nullam dignissim lorem diam, sed lacinia est efficitur nec. Fusce quis dolor at arcu volutpat accumsan. Quisque eu magna ipsum.In velit justo, tempor nec sem vel, hendrerit commodo dui. Fusce ut augue at elit euismod mattis et et urna. Proin sit amet dolor varius, porttitor sapien sed, mattis massa. Aenean ut erat felis. Donec vulputate aliquam tellus, et tempus nunc varius vitae. Curabitur arcu mi, tristique ut molestie ut, mollis a diam. Phasellus rutrum tincidunt neque nec convallis. Maecenas rutrum turpis sed laoreet rutrum. Mauris nisi nulla, vehicula sit amet commodo sit amet, volutpat at ex. Praesent et velit eros. Nulla gravida nisl quis vulputate cursus. In dapibus nisl sed massa egestas, quis dapibus purus facilisis. Fusce luctus varius justo, sit amet ullamcorper lacus porta eget. Suspendisse in lectus nec dui posuere cursus eget id enim. Donec porttitor sapien ut est tempus, a aliquet elit hendrerit. In scelerisque ac diam id ultricies. Duis imperdiet maximus tincidunt. Maecenas condimentum suscipit purus, sed dictum ipsum elementum quis. Integer ut venenatis est. Fusce et ornare massa.</p><h2 id="second-heading">Second Heading</h2><ul><li>Unordered lists, and:<ol><li>One</li><li>Two</li><li>Three</li></ol></li><li>More</li></ul><blockquote><p>Blockquote</p></blockquote><p>And <strong>bold</strong>, <em>italics</em>, and even <em>italics and later <strong>bold</strong></em>. Even <del>strikethrough</del>. <a href="https://markdowntohtml.com">A link</a> to somewhere.</p><p>And code highlighting:</p><pre><code class="lang-js"><span class="hljs-keyword">var</span> foo = <span class="hljs-string">'bar'</span>;<span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">baz</span><span class="hljs-params">(s)</span> </span>{<span class="hljs-keyword">return</span> foo + <span class="hljs-string">':'</span> + s;}</code></pre><p>Or inline code like <code>var foo=&#39;bar&#39;;</code>.</p><p>Or an image of bears</p><p><img src="http://placebear.com/200/200" alt="bears"></p><p>The end ...</p>`;
-
 	export const tutorial = [
 		{
-			title: 'Tutorial #1',
-			preview:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae gravida ligula, ac rhoncus magna. Donec vulputate tellus eu justo mattis ultrices. Aliquam lectus lacus, congue eget sollicitudin a, lacinia a risus. Sed vulputate facilisis luctus. Suspendisse vitae metus nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
-			content: placeholderText,
-			img: 'https://i.pinimg.com/originals/3e/d4/98/3ed498d3a4be00401e9bda4b5c68f237.jpg',
-			date: 'July 9, 2021'
+			title: 'How to create an account on Google Pay and connect it to your bank account.',
+			preview: '// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			content: `<ul><li>Step 1 : Open GPay on your mobile.</li><li>Step 2 : Enter the phone number that is linked to your bank account and click on <code>Next</code> at the bottom of the screen. </li><li>Step 3 : Select the email and the mobile number you wish to link to your bank account. Tap <code>Next</code>.</li><li>Step 4 : You will receive an OTP on your registered mobile number. Enter the OTP and press <code>Next</code>.</li><li>Step 5 : Click on create UPI pin and select <code>Continue</code>. </li><li>Step 6 : Set a strong pin. This pin is required whenever you open the app. Tap on ✅.</li><li>Step 7 : Now, tap on your profile picture at the top-right corner. </li><li>Step 8 : Click on <code>Add bank account</code>.</li><li>Step 9 : You will get a list of banks. Select the bank in which you have your account.</li><li>Step 10 : Once the bank is selected, a pop-up appears that asks to verify your number with the bank. Click on Continue.</li><li>Step 11 : GPay will send a verification code from your phone and it will auto-verify your number with the bank.</li><li>Step 12 : Now, you need to verify your account with the help of an ATM card. Click on the card and then <code>Start</code> at the bottom of the screen.</li><li>Step 13 : Enter the last 6 digits of your debit card, expiry date and click on ➡.</li><li>Step 14 : You will receive an OTP. Enter it and set a strong UPI pin. This will be required whenever you make a transaction.</li><li>Step 15 : Your account is created and linked to GPay.</li></ul>`,
+			img: 'https://images.unsplash.com/photo-1522937335625-b87ea99dc133?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+			date: 'July 19, 2021'
 		},
 		{
 			title: 'Tutorial #2',
-			preview:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae gravida ligula, ac rhoncus magna. Donec vulputate tellus eu justo mattis ultrices. Aliquam lectus lacus, congue eget sollicitudin a, lacinia a risus. Sed vulputate facilisis luctus. Suspendisse vitae metus nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
-			content: placeholderText,
+			preview: '// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			content: `placeholderText 1 123231 asdasd`,
 			img: 'https://i.pinimg.com/originals/3e/d4/98/3ed498d3a4be00401e9bda4b5c68f237.jpg',
-			date: 'July 9, 2021'
+			date: 'July 20, 2021'
 		},
 		{
 			title: 'Tutorial #3',
 			preview:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae gravida ligula, ac rhoncus magna. Donec vulputate tellus eu justo mattis ultrices. Aliquam lectus lacus, congue eget sollicitudin a, lacinia a risus. Sed vulputate facilisis luctus. Suspendisse vitae metus nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
-			content: placeholderText,
+				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			content: `placeholderText 1 asdas asdasd`,
 			img: 'https://i.pinimg.com/originals/3e/d4/98/3ed498d3a4be00401e9bda4b5c68f237.jpg',
 			date: 'July 9, 2021'
 		}
