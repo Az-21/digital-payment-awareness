3f9432b82ffd2dfca62046d3001543b2a2c64fc1
Az21 <39803478+Az-21@users.noreply.github.com>
2021-08-02 12:14:47 +0530

    🎈 relaxed preview requirement for tutorials

--- src/components/TextCard.svelte
+++ src/components/TextCard.svelte
@@ -8,7 +8,7 @@
 	// export let icon: string = 'fa-question';
 </script>
 
-<div class="mx-auto">
+<div class="mx-auto min-w-full">
 	<div class="bg-gray-900 shadow-md mb-6 tracking-wide">
 		<div class="md:flex-shrink-0">
 			<img src={img} alt="cover" class="w-full h-64 object-cover" />
@@ -19,14 +19,21 @@
 				<h2 class="font-bold text-lg tracking-normal h-12">{title}</h2>
 			</div>
 			<div class="pt-4 flex flex-row mt-2">
-					<i class="fas fa-clock {color} mr-2" />
-					<p class="text-gray-500 text-xs">{time}</p>
+				<i class="fas fa-clock {color} mr-2" />
+				<p class="text-gray-500 text-xs">{time}</p>
 			</div>
-			<p class="text-sm text-gray-300 h-32 overflow-hidden">{subtitle}</p>
-			<a href={routeID} class="cursor-pointer mt-4 flex items-center justify-center">
-				<i class="fas fa-angle-down {color} mr-2 transform scale-150" />
-				<p class="{color}">Read More</p>
-			</a>
+			{#if subtitle != '...'}
+				<p class="text-sm text-gray-300 h-32 overflow-hidden">{subtitle}</p>
+				<a href={routeID} class="cursor-pointer mt-4 flex items-center justify-center">
+					<i class="fas fa-angle-down {color} mr-2 transform scale-150" />
+					<p class={color}>Read More</p>
+				</a>
+			{:else}
+				<a href={routeID} class="cursor-pointer mt-2 flex items-center justify-center">
+					<i class="fas fa-angle-down {color} mr-2 transform scale-150" />
+					<p class={color}>Read</p>
+				</a>
+			{/if}
 		</div>
 	</div>
 </div>
--- src/content/TutorialJSON.svelte
+++ src/content/TutorialJSON.svelte
@@ -2,81 +2,83 @@
 	export const tutorial = [
 		{
 			title: 'How to create an account on Google Pay',
-			preview: '// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			preview: '',
 			content: `<ul><li>Step 1 : Open GPay on your mobile.</li><li>Step 2 : Enter the phone number that is linked to your bank account and click on <code>Next</code> at the bottom of the screen. </li><li>Step 3 : Select the email and the mobile number you wish to link to your bank account. Tap <code>Next</code>.</li><li>Step 4 : You will receive an OTP on your registered mobile number. Enter the OTP and press <code>Next</code>.</li><li>Step 5 : Click on create UPI pin and select <code>Continue</code>. </li><li>Step 6 : Set a strong pin. This pin is required whenever you open the app. Tap on ✅.</li><li>Step 7 : Now, tap on your profile picture at the top-right corner. </li><li>Step 8 : Click on <code>Add bank account</code>.</li><li>Step 9 : You will get a list of banks. Select the bank in which you have your account.</li><li>Step 10 : Once the bank is selected, a pop-up appears that asks to verify your number with the bank. Click on Continue.</li><li>Step 11 : GPay will send a verification code from your phone and it will auto-verify your number with the bank.</li><li>Step 12 : Now, you need to verify your account with the help of an ATM card. Click on the card and then <code>Start</code> at the bottom of the screen.</li><li>Step 13 : Enter the last 6 digits of your debit card, expiry date and click on ➡.</li><li>Step 14 : You will receive an OTP. Enter it and set a strong UPI pin. This will be required whenever you make a transaction.</li><li>Step 15 : Your account is created and linked to GPay.</li></ul>`,
-			img: 'https://images.unsplash.com/photo-1522937335625-b87ea99dc133?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+			img:
+				'https://images.unsplash.com/photo-1522937335625-b87ea99dc133?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
 			date: 'July 19, 2021'
 		},
 		{
-			title: 'How to share your UPI ID',
-			preview: '// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			title: 'How to share your UPI ID via scannable QR code',
+			preview: '',
 			content: `<ul><li>Step 1 : Click on your profile picture at the top-right of your screen.</li><li>Step 2 : Click on your profile picture again.</li><li>Step 3 : You will see a screen displaying your <code>Spot Code</code>. Spot Code is similar to a QR code, but is unique to Google Pay. <code>Scan my spot code to connect</code> will be written below the visual code.</li><li>Step 4 : Swipe right. </li><li>Step 5 : You will see a screen with your QR code displayed.</li><li>Step 6 : Below the QR code, you have your UPI ID. </li><li>Step 7 : Copy it and share your UPI ID.</li></ul>`,
-			img: 'https://images.unsplash.com/photo-1530989166579-96a5923c8ce5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80',
+			img:
+				'https://images.unsplash.com/photo-1530989166579-96a5923c8ce5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80',
 			date: 'July 20, 2021'
 		},
 		{
-			title: 'How to change UPI pin',
-			preview:
-				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			title: 'How to change six digit UPI pin',
+			preview: '',
 			content: `<ul><li>Step 1 : Click on your profile picture.</li><li>Step 2 : Click on <code>Bank Account</code> under the tab <code>Set up Payment Methods</code>. </li><li>Step 3 : Click on your bank account. </li><li>Step 4 : Tap on the three dots at the top-right corner of your screen.</li><li>Step 5 : Click on <code>Change UPI pin</code>.</li><li>Step 6 : Enter your old UPI pin and press ✅.</li><li>Step 7 : Enter the new pin you wish to keep and press ✅.</li><li>Step 8 : Enter the new pin again to confirm. </li><li>Step 9 : Your UPI pin is changed.</li></ul>`,
-			img: 'https://images.unsplash.com/photo-1504203700686-f21e703e5f1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1961&q=80',
+			img:
+				'https://images.unsplash.com/photo-1504203700686-f21e703e5f1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1961&q=80',
 			date: 'July 21, 2021'
 		},
 		{
-			title: 'How to check account balance',
-			preview:
-				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			title: 'How to check account balance of linked account(s)',
+			preview: '',
 			content: `<ul><li>Step 1 : Click on your profile picture at the top-right of your screen.</li><li>Step 2 : Click on <code>Bank Account</code> under the tab <code>Set up payment methods</code>.</li><li>Step 3 : Click on your bank account.</li><li>Step 4 : At the bottom of the options displayed, you will find <code>View Account Balance</code>. Select that.</li><li>Step 5 : GPay will prompt you to enter your 6-digit pin.</li><li>Step 6 : Enter your UPI pin and press ✅. </li><li>Step 7 : Your account and its balance will be displayed.</li></ul>`,
-			img: 'https://images.unsplash.com/photo-1565373679107-344d38dbf734?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+			img:
+				'https://images.unsplash.com/photo-1565373679107-344d38dbf734?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
 			date: 'July 22, 2021'
 		},
 		{
 			title: 'How to pay bills',
-			preview:
-				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			preview: '',
 			content: `<ul><li>Step 1 : Click on <code>New Payments</code> on the home screen of the app.</li><li>Step 2: Now, click on the <code>Bill Payments</code> option.</li><li>Step 3 : Click on <code>Electricity</code>.</li><li>Step 4 : Select your Electricity Supply Corporation. You will see that no account has been linked yet. </li><li>Step 5 : To link your account, Click on <code>Get Started</code>.</li><li>Step 6 : Enter your 10-digit Account ID. You can find this in your electricity bill.</li><li>Step 7 : Enter your <code>Account Name</code>. Note that this is for your own reference. You may, however, skip this field.</li><li>Step 8 : Check whether the information you have entered is correct.</li><li>Step 9 : Now, click on the <code>Link Account</code> option. Once this is done, you will see that your account has been linked and your most recent bill is displayed.</li><li>Step 10 : You can verify this amount with your electricity bill.</li><li>Step 11 : To make the payment, click on <code>Pay Bill</code>.</li><li>Step 12 : Select the bank account for making the payment and click on <code>Proceed to Pay</code>.</li><li>Step 13: Follow the steps of making a payment. This is similar to a normal transaction you would make.</li><li>Step 14: Your electricity bill is paid. Similarly, you can pay bills for other services like Cable, LPG, Broadband and many more. </li></ul>`,
-			img: 'https://images.unsplash.com/photo-1545941962-1b6654eb8072?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1900&q=80',
+			img:
+				'https://images.unsplash.com/photo-1545941962-1b6654eb8072?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1900&q=80',
 			date: 'July 23, 2021'
 		},
 		{
 			title: 'How to scan a QR code and make a payment',
-			preview:
-				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			preview: '',
 			content: `<ul><li>Step 1 : Click on <code>Scan any QR code</code> at the top-left corner.</li><li>Step 2 : Place the QR code in front of your camera to scan it. If it is too dark, you can turn on the flash by clicking on the torch symbol at the top-right corner.</li><li>Step 3 : Enter the amount to be paid. You might see a message asking to turn on <code>Location Services</code> for better results. You can either allow or click cancel.</li><li>Step 4 : You can add a note stating the purpose of the money transfer. However, this step is optional. </li><li>Step 5 : Tap on ➡ at the bottom of the page.</li><li>Step 6 : Choose the account you want to pay with.</li><li>Step 7 : Enter your UPI pin and click on ✅.</li></ul>`,
-			img: 'https://images.unsplash.com/photo-1559131397-f94da358f7ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+			img:
+				'https://images.unsplash.com/photo-1559131397-f94da358f7ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
 			date: 'July 24, 2021'
 		},
 		{
 			title: 'How to send money to contacts',
-			preview:
-				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			preview: '',
 			content: `<ul><li>Step 1: Click on <code>New payment</code> at the bottom of your screen.</li><li>Step 2: Click on the search bar at the top.</li><li>Step 3: Type the name or number of the person. </li><li>Step 4: Once you recognize the contact by checking the phone number, click on it.</li><li>Step 5: Tap <code>Pay</code> at the bottom of the page. You might see a message asking you to turn on <code>Location Services</code> for better results. You can either allow or click cancel.</li><li>Step 6: Type the amount you want to transfer.</li><li>Step 7 : You can attach a note stating the purpose for sending money. However, this is optional and you can leave it blank.</li><li>Step 8: Tap on ➡.</li><li>Step 9: Choose the account you want to pay with and click on <code>Pay</code>.</li><li>Step 10: Fill your UPI pin and click on ✅. </li></ul>`,
-			img: 'https://images.unsplash.com/photo-1529156069898-49953e39b3ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1789&q=80',
+			img:
+				'https://images.unsplash.com/photo-1529156069898-49953e39b3ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1789&q=80',
 			date: 'July 25, 2021'
 		},
 		{
 			title: 'How to add multiple bank accounts to your UPI ID',
-			preview:
-				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			preview: '',
 			content: `<h3 id="how-to-add-multiple-bank-accounts-to-your-upi-id-">How to add multiple bank accounts to your UPI ID.</h3><ul><li>Step 1 : Before adding a bank account to your UPI ID, ensure that your mobile number is linked to your bank account and you have your SIM installed on your current device.</li><li>Step 2 : Click on your profile picture on the top-right corner.</li><li>Step 3 : Click on bank account under <code>Set up payment methods</code>.</li><li>Step 4 : Click on <code>Add bank account</code>.</li><li>Step 5 : Choose the bank in which you have your account. You can also search by typing the name in the top bar.</li><li>Step 6 : Once the bank is selected, a pop-up appears that asks to verify your number with the bank. Click on <code>Continue</code></li><li>Step 7 : GPay will send a verification code from your phone and it will auto-verify.</li><li>Step 8 : After verification, your account will have a new bank account attached to your UPI ID.</li><li>Step 9 : Similarly, you can add other bank accounts as well to your UPI ID.</li></ul>`,
-			img: 'https://images.unsplash.com/photo-1462045504115-6c1d931f07d1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80',
+			img:
+				'https://images.unsplash.com/photo-1462045504115-6c1d931f07d1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80',
 			date: 'July 26, 2021'
 		},
 		{
 			title: 'How to pay others through UPI ID',
-			preview:
-				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			preview: '',
 			content: `<ul><li>Step 1 : Click on <code>New Payment</code> at the bottom of the screen.</li><li>Step 2 : Click on <code>UPI ID or QR</code> under the <code>Transfer Money</code> section.</li><li>Step 3 : You will get pop up having two options: Pay to <ul><li><code>UPI ID</code></li><li><code>Open code scanner</code></li></ul></li><li>Step 4 : Click on <code>UPI ID</code></li><li>Step 5 : Type the UPI ID of the person you wish to send the money to and click on <code>Verify</code>.</li><li>Step 6 : If the UPI ID is correct, then the name and profile picture of the person is displayed. Click <code>OK</code> if you intend to send money to that person. You might see a message asking you to turn on <code>Location Services</code> for better results. You can either allow or click cancel.</li><li>Step 7 : Click on <code>Pay</code> at the bottom.</li><li>Step 8 : Enter the amount to pay.</li><li>Step 9 : You can attach a note stating the purpose of sending the money. However, this is optional.</li><li>Step 10 : Tap on ➡.</li><li>Step 11: Choose the account you want to pay with and click on <code>Pay</code>.</li><li>Step 12: Enter your UPI pin and click on ✅.</li></ul>`,
-			img: 'https://images.unsplash.com/photo-1550482768-88b710a445fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80',
+			img:
+				'https://images.unsplash.com/photo-1550482768-88b710a445fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80',
 			date: 'July 27, 2021'
 		},
 		{
 			title: 'How to send money to online merchants (eg: Amazon)',
-			preview:
-				'// todo | 265 chars Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam volutpat, turpis sit amet mattis sagittis, orci erat aliquet diam, id consectetur metus enim ut augue. Aenean vitae nulla lacus.',
+			preview: '',
 			content: `<p>You can pay through GPay at different online sites by following the below steps:</p><ul><li>Step 1 : Select <code>Pay via UPI</code> in the online site/app you are using.</li><li>Step 2 : Click on GPay or Enter your UPI ID. <a href="https://digital-payment-awareness.vercel.app/tutorial/1">Here&#39;s how to get your UPI ID</a>.</li><li>Step 3 : You will get a message on your phone requesting for money. Check whether the amount that is being requested is the same as the amount for the item(s) mentioned in the website or app.</li><li>Step 4: If you didn&#39;t receive a notification, then go to GPay and check the notification center.</li><li>Step 4 : Click on the link given in the message for payment.</li><li>Step 5 : You can attach a note stating the purpose of sending the money. However, this is optional and you can choose to skip it.</li><li>Step 6 : Tap on ➡.</li><li>Step 7 : Choose the account you want to pay with and press <code>Pay</code></li><li>Step 8 : Enter your UPI pin and click ✅.</li></ul>`,
-			img: 'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1971&q=80',
+			img:
+				'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1971&q=80',
 			date: 'July 28, 2021'
-		},
+		}
 	];
 </script>
