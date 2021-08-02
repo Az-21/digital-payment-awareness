da599008522f387408b64403e02306fbc200e186
gaxrai <ganeahrai7@gmail.com>
2021-08-02 11:41:29 +0530

    fixed navbar breaking at 'md' breakpoint issue

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	export let shown = true;
+	export let shown = false;
 	function toggleMenu() {
 		shown = !shown;
 	}
 </script>
 
-<nav class="shadow bg-gray-900 fixed z-50 w-full">
+<nav class="shadow bg-gray-900 fixed z-10 w-full">
 	<div class="container px-6 py-2 mx-auto md:flex md:justify-between md:items-center">
 		<div class="flex items-center justify-between">
-			<div>
+			<div class="flex-grow text-center">
 				<p class="text-md font-medium uppercase md:place-content-center">
 					Digital Payment Awareness
 				</p>
@@ -24,7 +24,21 @@
 
 		<!-- Mobile Menu open: "block", Menu closed: "hidden" -->
 		{#if shown}
-			<div class="items-center md:flex">
+			<div class="flex justify-center items-center text-center md:flex">
+				<div class="flex flex-col md:flex-row md:mx-6">
+					<a class="text-sm font-medium hover:text-blue-500 md:mx-4 md:my-0" href="#blog">Blogs</a>
+					<a class="text-sm font-medium hover:text-yellow-500 md:mx-4 md:my-0" href="#tutorial"
+						>Tutorials</a
+					>
+					<a class="text-sm font-medium hover:text-red-500 md:mx-4 md:my-0" href="#video">Videos</a>
+					<a
+						class="text-sm font-medium hover:text-gray-400 md:mx-4 md:my-0"
+						href="https://github.com/Az-21/digital-payment-awareness">GitHub</a
+					>
+				</div>
+			</div>
+		{:else}
+			<div class="hidden md:flex">
 				<div class="flex flex-col md:flex-row md:mx-6">
 					<a class="text-sm font-medium hover:text-blue-500 md:mx-4 md:my-0" href="#blog">Blogs</a>
 					<a class="text-sm font-medium hover:text-yellow-500 md:mx-4 md:my-0" href="#tutorial"
