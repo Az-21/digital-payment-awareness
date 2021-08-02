e4c87d1f64e7dbfeeeec410230d2706b46880c97
Az21 <39803478+Az-21@users.noreply.github.com>
2021-08-02 11:58:06 +0530

    🏓 added more padding to section headers to prevent navbar overlap

--- src/components/SectionHeader.svelte
+++ src/components/SectionHeader.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="divide-y-4 {divColor} w-5/6 mx-auto">
-	<div class="flex text-3xl font-black pt-6 {textColor} uppercase">
+	<div class="flex text-3xl font-black pt-12 {textColor} uppercase">
 		{text}
 	</div>
 	<div class="pb-10"/>
